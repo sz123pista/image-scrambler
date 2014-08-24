85d2549b391b89037456ae6ef4653d72a2038efa
sz.pista <sz.pista@gmail.com>
2014-08-24 19:58:10 +0200

    Speeding up mapping with unused list and bisect.

--- scramble.py
+++ scramble.py
@@ -1,19 +1,20 @@
 import sys
 import os
 import hashlib
+import bisect
 from PIL import Image
 
-
 def point_mapping(hash_fun, image_len, hash_init_val):
-	used_points = list() #TODO: improve this!
+	unused_points = range(image_len)
 	last_hash = hash_init_val
 	for i in range(image_len):
 		act_hash = hash_fun(last_hash, i)
 		last_hash = act_hash
-		map_point = act_hash % image_len
-		while map_point in used_points:
-			map_point = (map_point + 1) % image_len
-		used_points.append(map_point)
+		j = act_hash % image_len
+		p = bisect.bisect_left(unused_points, j)
+		if p == len(unused_points):
+			p = bisect.bisect_left(unused_points, 0)
+		map_point = unused_points.pop(p)
 		yield map_point
 
 def hash_point(last_hash, in_point):
@@ -21,19 +22,19 @@ def hash_point(last_hash, in_point):
 	#return in_point * 1001230123231230123312L % 21201231233L
 
 def scramble(image_in):
-	in_list_len = len(image_in)
+	in_len = len(image_in)
 	image_out = list(image_in)
 	i = 0
-	for m in point_mapping(hash_point, in_list_len, 0):
+	for m in point_mapping(hash_point, in_len, 0):
 		image_out[m] = image_in[i]
 		i += 1
 	return image_out
 
 def descramble(image_in):
-	in_list_len = len(image_in)
+	in_len = len(image_in)
 	image_out = list(image_in)
 	i = 0
-	for m in point_mapping(hash_point, in_list_len, 0):
+	for m in point_mapping(hash_point, in_len, 0):
 		image_out[i] = image_in[m]
 		i += 1
 	return image_out
@@ -47,7 +48,7 @@ def main():
 	im3 = Image.new(im.mode, im.size)
 	image_data = im.getdata()
 
-	image_data_2 = scramble(list(image_data)[:1000])
+	image_data_2 = scramble(list(image_data))
 
 	im2.putdata(image_data_2)
 	im2.show()
