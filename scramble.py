bacd9ffc15d4e9f890f821c91ce7e5cb0cf2e18d
sz.pista <sz.pista@gmail.com>
2014-09-07 21:43:02 +0200

    Adding key image
    The upper half of the input image is mapped to the light points of the key
    image, and the lower half is mapped to the dark points. This way features
    of the key are visible on the scrambled output image.

--- scramble.py
+++ scramble.py
@@ -22,32 +22,54 @@ def hash_point(last_hash, in_point):
 	#return in_point * 1001230123231230123312L % 21201231233L
 	#return in_point * 200
 
-def scramble(image_in, hash_init_val, descramble=False):
+def process_key(key_in):
+	dark_points = list() #list of indices of dark points
+	light_points = list() #list of indices of light points
+	for i in range(len(key_in)):
+		if sum(key_in[i]) > 384:
+			light_points.append(i)
+		else:
+			dark_points.append(i)
+	return (light_points, dark_points)
+
+def scramble(image_in, key_in, hash_init_light, hash_init_dark, descramble=False):
 	in_len = len(image_in)
 	image_out = list(image_in)
+	(lp, dp) = process_key(key_in)
 	i = 0
-	for m in point_mapping(hash_point, in_len, hash_init_val):
+	#first map the upper half of the image to the light points of the key image
+	for m in point_mapping(hash_point, len(lp), hash_init_light):
 		if not descramble:
-			image_out[m] = image_in[i]
+			image_out[lp[m]] = image_in[i]
 		else:
-			image_out[i] = image_in[m]
+			image_out[i] = image_in[lp[m]]
+		i += 1
+	#then the lower half to the dark points of the key image
+	for m in point_mapping(hash_point, len(dp), hash_init_dark):
+		if not descramble:
+			image_out[dp[m]] = image_in[i]
+		else:
+			image_out[i] = image_in[dp[m]]
 		i += 1
 	return image_out
 
 def main():
-	if (2 != len(sys.argv)):
-		print "Usage: " + sys.argv[0] + " image"
+	if (3 != len(sys.argv)):
+		print "Usage: " + sys.argv[0] + " image key"
 		exit(0)
 	im_filename = sys.argv[1]
+	key_filename = sys.argv[2]
 	im = Image.open(im_filename, "r")
 	image_data = im.getdata()
+	key = Image.open(key_filename, "r")
+	key_data = key.getdata()
 
-	image_data_2 = scramble(list(image_data), 0)
+	image_data_2 = scramble(image_data, key_data, 0, 10)
 	im2 = Image.new(im.mode, im.size)
 	im2.putdata(image_data_2)
 	im2.show()
 
-	image_data_3 = scramble(list(image_data_2), 0, True)
+	image_data_3 = scramble(image_data_2, key_data, 0, 10, True)
 	im3 = Image.new(im.mode, im.size)
 	im3.putdata(image_data_3)
 	im3.show()
